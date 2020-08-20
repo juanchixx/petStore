50b0f3bacc886ac0ad7bc03e2d6736c7a3523811
juanchixx <juanchixx@gmail.com>
2020-08-20 20:31:18 -0300

    Se cambia la ruta del Logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom'
 
-import logo from '../paw.svg'
+import logo from '../images/paw.svg'
 
 export function NavBar(){
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#"> <img src='{logo}' alt='logo'/> </a>
+            <a class="navbar-brand" href="#"> <img src={logo} alt='logo' width="30" height="30" className='d-inline-block align-top' loading="lazy"/> </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
