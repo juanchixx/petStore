684b5e46664ec310c3731097a1ef89ac32b8265a
juanchixx <juanchixx@gmail.com>
2020-08-26 20:53:11 -0300

    Se agrega contador con hooks

---
+++ src/components/ItemCount.js
@@ -0,0 +1,36 @@
+import React,  {useState} from 'react'
+
+let min = 0;
+let max = 0;
+
+export default function ItemCount(props){
+    
+    min = props.min;
+    max = props.max;
+
+    let [count, setCount] = useState(1);
+
+    function Add(){
+        if(count < max)
+            setCount( count + 1);
+    }
+    function Substract(){
+        if(count > min)
+            setCount( count - 1);
+    }
+    
+    return(
+        <div className="input-group mb-3">
+            <div className="input-group-prepend">
+                <button onClick={Substract} className="btn btn-outline-secondary" type="button" id="button-addon1">-</button>
+            </div>
+            <input type="text" className="form-control text-center" placeholder="" aria-label="Example text with button addon" value={count} aria-describedby="button-addon1"/>
+            <div className="input-group-prepend">
+                <button onClick={Add} className="btn btn-outline-secondary" type="button" id="button-addon1">+</button>
+            </div>
+        </div>
+    )   
+}
+
+
+
--- src/views/home.js
+++ src/views/home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Welcome from '../components/welcome'
+import ItemCount from '../components/ItemCount'
 
-var contador = 0;
 
 function Home(){
+
     return(
         <div className="container">
             <h1>
@@ -14,19 +15,12 @@ function Home(){
                     <div className="card-body">
                         <h5 className="card-title">Comida para Perros</h5>
                         <p className="card-text">Pedigree</p>
-                        <a onClick={sumarItem} href="#" className="btn btn-primary">Comprar</a>
+                        <ItemCount min={1} max={6} />
                     </div>     
                 </div>             
             
         </div>
     );
-}
-
-function sumarItem(event){
-    event.preventDefault();
-    contador++;
-    var contador_label = document.getElementById('contador');
-    contador_label.innerText = contador.toString();
 }
 
 export default Home;
