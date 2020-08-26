a2920f674b70977db628b9fc796028c9fb70c23d
juanchixx <juanchixx@gmail.com>
2020-08-25 22:38:41 -0300

    Se agrega un sumador de ejemplo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,9 +7,11 @@ import CartIcon from '../images/CartIcon'
 export function NavBar(){
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="/" > <img src={logo} alt='logo' width="30" height="30" className='d-inline-block align-top' loading="lazy"/> </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <NavLink to='/' className="navbar-brand">
+                 <img src={logo} alt='logo' width="30" height="30" className='d-inline-block align-top' loading="lazy"/>
+            </NavLink>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ml-auto">
@@ -26,6 +28,7 @@ export function NavBar(){
                     <li className="nav-item">
                         <NavLink to='/carrito' className='nav-link' activeClassName='active'>
                             <CartIcon />
+                            <span id='contador' className='badge badge-pill badge-success'></span>
                         </NavLink>
                     </li>
                 </ul>
--- src/views/home.js
+++ src/views/home.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import Welcome from '../components/welcome'
 
+var contador = 0;
+
 function Home(){
     return(
         <div className="container">
             <h1>
                 <Welcome nombre='Juan' app='petStore'/>
             </h1>
+                <div className="card" style={{width: "18rem"}}>
+                    <img className="card-img-top" alt="..." src = 'https://walmartar.vteximg.com.br/arquivos/ids/810666-292-292/Alimento-Perro-Razas-Grandes-Carne-Pedigree-100-Gr-1-23631.jpg?v=636372742221370000'/>
+                    <div className="card-body">
+                        <h5 className="card-title">Comida para Perros</h5>
+                        <p className="card-text">Pedigree</p>
+                        <a onClick={sumarItem} href="#" className="btn btn-primary">Comprar</a>
+                    </div>     
+                </div>             
+            
         </div>
     );
+}
+
+function sumarItem(event){
+    event.preventDefault();
+    contador++;
+    var contador_label = document.getElementById('contador');
+    contador_label.innerText = contador.toString();
 }
 
 export default Home;
