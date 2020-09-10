a58b276ff104a074fa55372e4a114782245e405c
juanchixx <juanchixx@gmail.com>
2020-09-10 20:31:57 -0300

    Ajustes al traer las Ofertas

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -6,11 +6,11 @@ export default function ItemDetail(props) {
     return(
 
         <div className="card" style={{width: "18rem"}}>
-            <img className="card-img-top" alt="..." src = 'https://walmartar.vteximg.com.br/arquivos/ids/810666-292-292/Alimento-Perro-Razas-Grandes-Carne-Pedigree-100-Gr-1-23631.jpg?v=636372742221370000'/>
+            <img className="card-img-top" alt="..." src = {props.img}/>
             <div className="card-body">
                 <h5 className="card-title">{props.title}</h5>
                 <p className="card-text">{props.description}</p>
-                <ItemCount initial={2} min={1} max={6} />
+                <ItemCount initial={props.initial} min={props.min} max={props.max} />
                 <button id={props.id} className='btn btn-primary'>Comprar</button>
             </div>     
         </div>   
--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -10,15 +10,15 @@ export default function ItemDetailContainer(props) {
         new Promise((resolve, reject) =>{
             setTimeout(() =>{
                     const data = [
-                        { id: 1, title:'Pecheras', description:'Ajustables para todos los perros'},
-                        { id: 2, title:'Correa', description:'gran variedad de colores'},
-                        { id: 3, title:'Cepillo',description:''}
+                        { id: 1, title:'Pecheras', description:'Ajustables para todos los perros', initial: 0, min: 0, max:3, img:'https://images-na.ssl-images-amazon.com/images/I/61J1jC1qGjL._AC_SX522_.jpg'},
+                        { id: 2, title:'Correa', description:'gran variedad de colores', initial: 0, min: 0, max:4, img:'https://deperroaperro.com/wp-content/uploads/2019/02/hermosas-correas-estampadas-disenos-unicos-perro-mascota-d_nq_np_692959-mla27019452443_032018-f.jpg'},
+                        { id: 3, title:'Cepillo',description:'', initial: 0, min: 0, max:15, img:'https://d26lpennugtm8s.cloudfront.net/stores/608/745/products/9b1abda9-b6cb-42df-ba45-bed3576e277c1-14a30d090132e82a3c15405160982651-640-0.jpeg'}
                     ]
                     resolve(data);
                 }, 3000);
         })
         .then(response => {
-        const dat = response.map((item) => <ItemDetail id={item.id} title={item.title} description={item.description} />);
+        const dat = response.map((item) => <ItemDetail id={item.id} title={item.title} description={item.description} initial={item.initial} min={item.min} max={item.max} img={item.img}/>);
             setData(dat);
             setLoading(false);
         })
